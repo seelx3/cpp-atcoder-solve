5b26cf87f06d8a165a7f7f386254a782ef6e3473
seelx3 <yuuhashi.26@gmail.com>
2022-07-30 02:19:00 +0900

    fix: lib dijkstra

--- lib/dijkstra.cpp
+++ lib/dijkstra.cpp
@@ -1,53 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+using ll = long long;
+#define ALL(v) v.begin(), v.end()
+#define sz(v) (int)v.size()
 const ll INF = 1e18;
 
-struct edge{ll to, cost;};
-typedef pair<ll,ll> P;
-struct graph{
-    ll V;
-    vector<vector<edge> > G;
-    vector<ll> d;
+struct edge {
+  ll to, cost;
+};
+typedef pair<ll, ll> P;
+struct graph {
+  ll V;
+  vector<vector<edge>> G;
+  vector<ll> d;
 
-    graph(ll n){
-        init(n);
-    }
+  graph(ll n) { init(n); }
 
-    void init(ll n){
-        V = n;
-        G.resize(V);
-        d.resize(V);
-        for(ll i = 0; i < V; i++){
-        d[i] = INF;
-        }
+  void init(ll n) {
+    V = n;
+    G.resize(V);
+    d.resize(V);
+    for (ll i = 0; i < V; i++) {
+      d[i] = INF;
     }
+  }
 
-    void add_edge(ll s, ll t, ll cost){
-        edge e;
-        e.to = t, e.cost = cost;
-        G[s].push_back(e);
-    }
+  void add_edge(ll s, ll t, ll cost) {
+    edge e;
+    e.to = t, e.cost = cost;
+    G[s].push_back(e);
+  }
 
-    void dijkstra(ll s){
-        for(ll i = 0; i < V; i++){
-        d[i] = INF;
-        }
-        d[s] = 0;
-        priority_queue<P,vector<P>, greater<P> > que;
-        que.push(P(0,s));
-        while(!que.empty()){
-        P p = que.top(); que.pop();
-        ll v = p.second;
-        if(d[v]<p.first) continue;
-        for(auto e : G[v]){
-            if(d[e.to]>d[v]+e.cost){
-            d[e.to] = d[v]+e.cost;
-            que.push(P(d[e.to],e.to));
-            }
-        }
-        }
+  void dijkstra(ll s) {
+    for (ll i = 0; i < V; i++) {
+      d[i] = INF;
     }
+    d[s] = 0;
+    priority_queue<P, vector<P>, greater<P>> que;
+    que.push(P(0, s));
+    while (!que.empty()) {
+      P p = que.top();
+      que.pop();
+      ll v = p.second;
+      if (d[v] < p.first)
+        continue;
+      for (auto e : G[v]) {
+        if (d[e.to] > d[v] + e.cost) {
+          d[e.to] = d[v] + e.cost;
+          que.push(P(d[e.to], e.to));
+        }
+      }
+    }
+  }
 };
 
 // how to use
