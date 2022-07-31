b3115a1336bf50a33f1da98fb9ad6b827e8842c6
seelx3 <yuuhashi.26@gmail.com>
2022-08-01 00:03:18 +0900

    solution: abc241 e

--- abc241/e.cpp
+++ abc241/e.cpp
@@ -1,19 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define chmin(a, b) a = min(a, b)
-#define chmax(a, b) a = max(a, b)
-#define ALL(v) v.begin(),v.end()
-#define sz(v) (ll)v.size()
-const ll INF = LONG_LONG_MAX;
-// const ll MOD = 998244353;
-const ll MOD = 1000000007;
-ll dx[] = {1, 0, -1, 0};
-ll dy[] = {0, 1, 0, -1};
-const double PI = acos(-1.0);
+#define ALL(v) v.begin(), v.end()
+#define sz(v) (int)v.size()
 
-/*---------------------------
----------------------------*/
+int main() {
+  ll n, k;
+  cin >> n >> k;
+  vector<ll> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
 
-int main(){
+  vector<ll> sum(n);
+  vector<ll> seen(n, -1);
+  seen[0] = 0;
+  ll s1, s2;
+
+  for (int i = 0; i < n; i++) {
+    sum[i + 1] = sum[i] + a[sum[i] % n];
+    if (seen[sum[i + 1] % n] != -1) {
+      s1 = seen[sum[i + 1] % n];
+      s2 = i + 1;
+      break;
+    }
+    seen[sum[i + 1] % n] = i + 1;
+  }
+
+  // cout << "s1 = " << s1 << endl;
+  // cout << "s2 = " << s2 << endl;
+
+  if (k <= s1) {
+    cout << sum[k] << endl;
+  } else {
+    ll lp_sz = s2 - s1;
+    ll lp_gain = sum[s2] - sum[s1];
+    ll lp_cnt = (k - s1) / lp_sz;
+    ll rest = (k - s1) % lp_sz;
+    // cout << "lp_cnt = " << lp_cnt << endl;
+    // cout << "rest = " << rest << endl;
+    cout << sum[s1 + rest] + lp_cnt * lp_gain << endl;
+  }
 }
