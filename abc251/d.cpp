512cab6e9483505ce73fad08a029e15767386151
seelx3 <yuuhashi.26@gmail.com>
2022-08-04 03:15:00 +0900

    solution: abc251 d

--- abc251/d.cpp
+++ abc251/d.cpp
@@ -1,45 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define ALL(v) v.begin(),v.end()
+#define ALL(v) v.begin(), v.end()
 #define sz(v) (int)v.size()
 
-int main(){
+int main() {
   ll w;
   cin >> w;
 
-  set<ll> st;
-  vector<ll> a;
+  vector<ll> ans;
+  ll a[] = {1, 100, 10000};
 
-  for(int i = 1; i <= 1000000; i++) {
-    if(st.count(i)) continue;
-    if(sz(a) == 300) break;
-    st.insert(i);
-    a.push_back(i);
-
-    ll n = sz(a);
-
-    for(int i = 0; i < n; i++) {
-      st.insert(a[i]);
-    }
-    
-    for(int i = 0; i < n-1; i++) {
-      for(int j = i+1; j < n; j++) {
-        st.insert(a[i]+a[j]);
-      }
-    }
-
-    for(int i = 0; i < n-2; i++) {
-      for(int j = i+1; j < n-1; j++) {
-        for(int k = j+1; k < n; k++) {
-          st.insert(a[i]+a[j]+a[k]);
-        }
-      }
+  for (int i = 0; i < 3; i++) {
+    for (int j = 1; j < 100; j++) {
+      ans.push_back(a[i] * j);
     }
   }
+  ans.push_back(1000000);
 
-  cout << sz(a) << endl;
-  for(int i = 0; i < sz(a); i++) {
-    cout << a[i] << " \n"[i==sz(a)-1];
+  cout << sz(ans) << endl;
+  for (int i = 0; i < sz(ans); i++) {
+    cout << ans[i] << " \n"[i == sz(ans) - 1];
   }
 }
