1f2b15e978cf422e9f6c38f23ca768fc32ead5f6
seelx3 <yuuhashi.26@gmail.com>
2022-08-20 11:28:55 +0900

    fix: modpow

--- lib/modpow.cpp
+++ lib/modpow.cpp
@@ -4,13 +4,13 @@ using namespace std;
 //繰り返し2乗法　計算量O(log N)
 
 long long modpow(long long x, long long n, long long m) {
-    long long ret = 1;
-    while (n > 0) {
-        if (n & 1) ret = ret * x % m;
-        x = x * x % m;
-        n >>= 1; 
-    }
-    return ret;
+  x %= m;
+  long long ret = 1;
+  while (n > 0) {
+    if (n & 1)
+      ret = ret * x % m;
+    x = x * x % m;
+    n >>= 1;
+  }
+  return ret;
 }
-
-
