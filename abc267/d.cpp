3a43b828b8ea107a1bb05ea2ba739081388b0e52
seelx3 <yuuhashi.26@gmail.com>
2022-09-03 23:37:42 +0900

    solution: abc267

---
+++ abc267/d.cpp
@@ -0,0 +1,76 @@
+#ifndef INCLUDED_MAIN
+#define INCLUDED_MAIN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+#define ALL(v) v.begin(), v.end()
+#define SZ(v) (int)(v.size())
+
+#include __FILE__
+
+const ll INF = 1000000000000000;
+
+int main() {
+  ll n = input();
+  ll m = input();
+  vector<ll> a(n + 1);
+  REP(i, 1, n + 1) cin >> a[i];
+
+  vector<vector<ll>> dp(n + 1, vector<ll>(m + 1, -INF));
+  for (int i = 0; i < n + 1; i++)
+    dp[i][0] = 0;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1] + j * a[i]);
+    }
+  }
+
+  deb(dp[1][0]);
+  cout << dp[n][m] << endl;
+}
+
+/*-----------------------------------------------------------
+-----------------------------------------------------------*/
+
+#else // INCLUDED_MAIN
+
+#define OVERLOAD_REP(_1, _2, _3, _4, name, ...) name
+#define REP1(i, n) REP3(i, 0, n, 1)
+#define REP2(i, a, b) REP3(i, a, b, 1)
+#define REP3(i, a, b, c) for (int i = int(a); i < int(b); i += c)
+#define REP(...) OVERLOAD_REP(__VA_ARGS__, REP3, REP2, REP1, )(__VA_ARGS__)
+
+#define OVERLOAD_REPR(_1, _2, _3, _4, name, ...) name
+#define REPR1(i, n) REPR3(i, 0, n, 1)
+#define REPR2(i, a, b) REPR3(i, a, b, 1)
+#define REPR3(i, a, b, c) for (int i = int(b) - 1; i >= int(a); i -= c)
+#define REPR(...) OVERLOAD_REPR(__VA_ARGS__, REPR3, REPR2, REPR1, )(__VA_ARGS__)
+
+// clang-format off
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+template <class T> vector<T> make_vec(size_t a, T init) { return vector<T>(a, init); }
+template <class T, class... Ts> auto make_vec(size_t a, Ts... ts) {
+  return vector<decltype(make_vec<T>(ts...))>(a, make_vec<T>(ts...));
+}
+
+struct input { 
+  int n; input() {} input(int n_) : n(n_){};
+  template <class T> operator T() { T ret; cin >> ret; return ret; }
+  template <class T> operator vector<T>() { vector<T> ret(n); for (int i = 0; i < n; i++) cin >> ret[i]; return ret; }
+};
+// clang-format on
+
+#ifdef LOCAL
+#include <debug.hpp>
+#define deb(...) debug::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define deb(...) (static_cast<void>(0))
+#endif
+
+#endif // INCLUDED_MAIN
