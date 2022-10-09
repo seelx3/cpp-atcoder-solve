c5246e085a43a7714b0c97ecec2355994e8bb624
seelx3 <yuuhashi.26@gmail.com>
2022-10-10 01:08:10 +0900

    solution: abc272

---
+++ abc272/d.cpp
@@ -0,0 +1,108 @@
+#ifndef INCLUDED_MAIN
+#define INCLUDED_MAIN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+#define ALL(v) v.begin(), v.end()
+#define SZ(v) (int)(v.size())
+
+#include __FILE__
+
+int main() {
+  ll N, M;
+  cin >> N >> M;
+  vector<ll> dx, dy;
+  REPE(i, -N - 1, N + 1) {
+    REPE(j, -N - 1, N + 1) {
+      if (i * i + j * j == M) {
+        dx.push_back(i);
+        dy.push_back(j);
+      }
+    }
+  }
+  deb(dx);
+  deb(dy);
+  vector<vector<ll>> ans = make_vec(N, N, -1LL);
+  ans[0][0] = 0;
+  queue<pair<ll, ll>> qu;
+  qu.emplace(0, 0);
+
+  while (!qu.empty()) {
+    auto [x, y] = qu.front();
+    qu.pop();
+    REP(i, SZ(dx)) {
+      ll nx = x + dx[i];
+      ll ny = y + dy[i];
+      if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+      if (ans[nx][ny] != -1) continue;
+      ans[nx][ny] = ans[x][y] + 1;
+      qu.emplace(nx, ny);
+    }
+  }
+
+  REP(i, N) REP(j, N) { cout << ans[i][j] << " \n"[j == N - 1]; }
+}
+
+/*-----------------------------------------------------------
+-----------------------------------------------------------*/
+
+#else // INCLUDED_MAIN
+
+// clang-format off
+#define cauto const auto&
+
+#define OVERLOAD_REP(_1, _2, _3, _4, name, ...) name
+#define REP1(i, n) REP3(i, 0, n, 1)
+#define REP2(i, a, b) REP3(i, a, b, 1)
+#define REP3(i, a, b, c) for (int i = int(a); i < int(b); i += c)
+#define REP(...) OVERLOAD_REP(__VA_ARGS__, REP3, REP2, REP1, )(__VA_ARGS__)
+
+#define OVERLOAD_REPR(_1, _2, _3, _4, name, ...) name
+#define REPR1(i, n) REPR3(i, 0, n, 1)
+#define REPR2(i, a, b) REPR3(i, a, b, 1)
+#define REPR3(i, a, b, c) for (int i = int(b) - 1; i >= int(a); i -= c)
+#define REPR(...) OVERLOAD_REPR(__VA_ARGS__, REPR3, REPR2, REPR1, )(__VA_ARGS__)
+
+#define OVERLOAD_REPE(_1, _2, _3, _4, name, ...) name
+#define REPE1(i, n) REPE3(i, 0, n, 1)
+#define REPE2(i, a, b) REPE3(i, a, b, 1)
+#define REPE3(i, a, b, c) for (int i = int(a); i <= int(b); i += c)
+#define REPE(...) OVERLOAD_REPE(__VA_ARGS__, REPE3, REPE2, REPE1, )(__VA_ARGS__)
+
+#define OVERLOAD_REPRE(_1, _2, _3, _4, name, ...) name
+#define REPRE1(i, n) REPRE3(i, 0, n, 1)
+#define REPRE2(i, a, b) REPRE3(i, a, b, 1)
+#define REPRE3(i, a, b, c) for (int i = int(b); i >= int(a); i -= c)
+#define REPRE(...) OVERLOAD_REPRE(__VA_ARGS__, REPRE3, REPRE2, REPE1, )(__VA_ARGS__)
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+template< typename T, typename U >
+static inline vector<U> make_vec(T&& n, U&& val) noexcept {
+  return vector<U>(forward<T>(n), forward<U>(val)); 
+}
+template< typename T, typename... Args >
+static inline decltype(auto) make_vec(T&& n, Args&&... args) noexcept {
+  return vector<decltype(make_vec(forward<Args>(args)...))>(forward<T>(n), make_vec(forward<Args>(args)...));
+}
+
+struct input { 
+  int n; input() {} input(int n_) : n(n_){};
+  template <class T> operator T() { T ret; cin >> ret; return ret; }
+  template <class T> operator vector<T>() { vector<T> ret(n); for (int i = 0; i < n; i++) cin >> ret[i]; return ret; }
+};
+// clang-format on
+
+#ifdef LOCAL
+#include <debug.hpp>
+#define deb(...) debug::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define deb(...) (static_cast<void>(0))
+#endif
+
+#endif // INCLUDED_MAIN
---
+++ abc272/e.cpp
@@ -0,0 +1,109 @@
+#ifndef INCLUDED_MAIN
+#define INCLUDED_MAIN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+#define ALL(v) v.begin(), v.end()
+#define SZ(v) (int)(v.size())
+
+#include __FILE__
+
+int main() {
+  ll N, M;
+  cin >> N >> M;
+
+  vector<ll> A = input(N);
+
+  // v[i]: Aにi回操作を行ったときのAの要素の値で0~Nの範囲に含まれる値の集合
+  // 調和級数より、v[i]に含まれる要素数の和は N log N くらい
+  vector<vector<int>> v(M + 1);
+
+  REP(i, N) {
+    int l = (A[i] >= 0 ? 0 : (abs(A[i]) + i) / (i + 1));
+
+    for (int j = l; A[i] + (i + 1) * j <= N && j <= M; j++) {
+      v[j].push_back(A[i] + (i + 1) * j);
+    }
+  }
+
+  deb(v);
+
+  vector<int> ans;
+
+  REPE(i, 1, M) {
+    vector<bool> b(SZ(v[i]) + 1);
+    REP(j, SZ(v[i])) if (v[i][j] <= SZ(v[i])) b[v[i][j]] = true;
+    REP(j, SZ(v[i]) + 1) {
+      if (!b[j]) {
+        ans.push_back(j);
+        break;
+      }
+    }
+  }
+
+  REP(i, SZ(ans)) cout << ans[i] << '\n';
+}
+
+/*-----------------------------------------------------------
+-----------------------------------------------------------*/
+
+#else // INCLUDED_MAIN
+
+// clang-format off
+#define cauto const auto&
+
+#define OVERLOAD_REP(_1, _2, _3, _4, name, ...) name
+#define REP1(i, n) REP3(i, 0, n, 1)
+#define REP2(i, a, b) REP3(i, a, b, 1)
+#define REP3(i, a, b, c) for (int i = int(a); i < int(b); i += c)
+#define REP(...) OVERLOAD_REP(__VA_ARGS__, REP3, REP2, REP1, )(__VA_ARGS__)
+
+#define OVERLOAD_REPR(_1, _2, _3, _4, name, ...) name
+#define REPR1(i, n) REPR3(i, 0, n, 1)
+#define REPR2(i, a, b) REPR3(i, a, b, 1)
+#define REPR3(i, a, b, c) for (int i = int(b) - 1; i >= int(a); i -= c)
+#define REPR(...) OVERLOAD_REPR(__VA_ARGS__, REPR3, REPR2, REPR1, )(__VA_ARGS__)
+
+#define OVERLOAD_REPE(_1, _2, _3, _4, name, ...) name
+#define REPE1(i, n) REPE3(i, 0, n, 1)
+#define REPE2(i, a, b) REPE3(i, a, b, 1)
+#define REPE3(i, a, b, c) for (int i = int(a); i <= int(b); i += c)
+#define REPE(...) OVERLOAD_REPE(__VA_ARGS__, REPE3, REPE2, REPE1, )(__VA_ARGS__)
+
+#define OVERLOAD_REPRE(_1, _2, _3, _4, name, ...) name
+#define REPRE1(i, n) REPRE3(i, 0, n, 1)
+#define REPRE2(i, a, b) REPRE3(i, a, b, 1)
+#define REPRE3(i, a, b, c) for (int i = int(b); i >= int(a); i -= c)
+#define REPRE(...) OVERLOAD_REPRE(__VA_ARGS__, REPRE3, REPRE2, REPE1, )(__VA_ARGS__)
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+template< typename T, typename U >
+static inline vector<U> make_vec(T&& n, U&& val) noexcept {
+  return vector<U>(forward<T>(n), forward<U>(val)); 
+}
+template< typename T, typename... Args >
+static inline decltype(auto) make_vec(T&& n, Args&&... args) noexcept {
+  return vector<decltype(make_vec(forward<Args>(args)...))>(forward<T>(n), make_vec(forward<Args>(args)...));
+}
+
+struct input { 
+  int n; input() {} input(int n_) : n(n_){};
+  template <class T> operator T() { T ret; cin >> ret; return ret; }
+  template <class T> operator vector<T>() { vector<T> ret(n); for (int i = 0; i < n; i++) cin >> ret[i]; return ret; }
+};
+// clang-format on
+
+#ifdef LOCAL
+#include <debug.hpp>
+#define deb(...) debug::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define deb(...) (static_cast<void>(0))
+#endif
+
+#endif // INCLUDED_MAIN
