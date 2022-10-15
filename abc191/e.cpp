5b009737ec0d2f9676f05b5356442224573e1256
seelx3 <yuuhashi.26@gmail.com>
2022-10-15 20:39:57 +0900

    .

---
+++ abc191/e.cpp
@@ -0,0 +1,126 @@
+#ifndef INCLUDED_MAIN
+#define INCLUDED_MAIN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+#define ALL(v) v.begin(), v.end()
+#define SZ(v) (int)(v.size())
+
+#include __FILE__
+
+#define INF (ll)1e18
+
+struct Edge {
+  int to;
+  ll cost;
+};
+using Graph = vector<vector<Edge>>;
+using P = pair<ll, int>;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+  Graph G(N);
+  REP(_, M) {
+    int a, b;
+    ll c;
+    cin >> a >> b >> c;
+    a--, b--;
+    G[a].push_back(Edge{b, c});
+  }
+
+  vector<ll> ans(N, -1);
+
+  REP(s, N) {
+    deb(s);
+    priority_queue<P, vector<P>, greater<>> pq;
+    vector<ll> dist(N, INF);
+    dist[s] = 0;
+    pq.emplace(dist[s], s);
+    while (!pq.empty()) {
+      auto [d, u] = pq.top();
+      if (d != 0 && u == s) {
+        ans[s] = d;
+        break;
+      }
+      deb(d, u);
+      pq.pop();
+      for (auto e : G[u]) {
+        if (u == s && e.to == s) {
+          pq.emplace(e.cost, s);
+          continue;
+        }
+        if (dist[e.to] < d + e.cost) continue;
+        dist[e.to] = d + e.cost;
+        pq.emplace(d + e.cost, e.to);
+      }
+      if (u == s) dist[u] = INF;
+    }
+  }
+
+  REP(i, N) cout << ans[i] << '\n';
+}
+
+/*-----------------------------------------------------------
+-----------------------------------------------------------*/
+
+#else // INCLUDED_MAIN
+
+// clang-format off
+#define cauto const auto&
+
+#define OVERLOAD_REP(_1, _2, _3, _4, name, ...) name
+#define REP1(i, n) REP3(i, 0, n, 1)
+#define REP2(i, a, b) REP3(i, a, b, 1)
+#define REP3(i, a, b, c) for (int i = int(a); i < int(b); i += c)
+#define REP(...) OVERLOAD_REP(__VA_ARGS__, REP3, REP2, REP1, )(__VA_ARGS__)
+
+#define OVERLOAD_REPR(_1, _2, _3, _4, name, ...) name
+#define REPR1(i, n) REPR3(i, 0, n, 1)
+#define REPR2(i, a, b) REPR3(i, a, b, 1)
+#define REPR3(i, a, b, c) for (int i = int(b) - 1; i >= int(a); i -= c)
+#define REPR(...) OVERLOAD_REPR(__VA_ARGS__, REPR3, REPR2, REPR1, )(__VA_ARGS__)
+
+#define OVERLOAD_REPE(_1, _2, _3, _4, name, ...) name
+#define REPE1(i, n) REPE3(i, 0, n, 1)
+#define REPE2(i, a, b) REPE3(i, a, b, 1)
+#define REPE3(i, a, b, c) for (int i = int(a); i <= int(b); i += c)
+#define REPE(...) OVERLOAD_REPE(__VA_ARGS__, REPE3, REPE2, REPE1, )(__VA_ARGS__)
+
+#define OVERLOAD_REPRE(_1, _2, _3, _4, name, ...) name
+#define REPRE1(i, n) REPRE3(i, 0, n, 1)
+#define REPRE2(i, a, b) REPRE3(i, a, b, 1)
+#define REPRE3(i, a, b, c) for (int i = int(b); i >= int(a); i -= c)
+#define REPRE(...) OVERLOAD_REPRE(__VA_ARGS__, REPRE3, REPRE2, REPE1, )(__VA_ARGS__)
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+template< typename T, typename U >
+static inline vector<U> make_vec(T&& n, U&& val) noexcept {
+  return vector<U>(forward<T>(n), forward<U>(val)); 
+}
+template< typename T, typename... Args >
+static inline decltype(auto) make_vec(T&& n, Args&&... args) noexcept {
+  return vector<decltype(make_vec(forward<Args>(args)...))>(forward<T>(n), make_vec(forward<Args>(args)...));
+}
+
+struct input { 
+  int n; input() {} input(int n_) : n(n_){};
+  template <class T> operator T() { T ret; cin >> ret; return ret; }
+  template <class T> operator vector<T>() { vector<T> ret(n); for (int i = 0; i < n; i++) cin >> ret[i]; return ret; }
+};
+// clang-format on
+
+#ifdef LOCAL
+#include <debug.hpp>
+#define deb(...) debug::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define deb(...) (static_cast<void>(0))
+#endif
+
+#endif // INCLUDED_MAIN
